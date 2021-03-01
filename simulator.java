5ae71b150865c393401f64e1cc085de583360ce8
CS19B010Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2021-03-01 18:27:15 +0530

    Update simulator.java

--- simulator.java
+++ simulator.java
@@ -1,33 +1,46 @@
 package co_project;
 import java.io.*;
 import java.util.*;
+import java.util.*;
+
+import javax.swing.JFileChooser;
 public class simulator 
 {
    public static void main(String[] args) 
    {
-	Scanner sc=new Scanner(System.in);
 	int R[]=new int[32];
        int Mem[]=new int[1024];
        int PC=0;
-       File file = new File("./cs19b010_11.asm");
-       BufferedReader reader;
-       try 
-       {
-            reader = new BufferedReader(new FileReader(file));
-	    String line = reader.readLine();
-	    while (line != null) 
-            {
-		PC=PC+4;
-		System.out.println(line);
-		line = reader.readLine();
-	    }
-	   System.out.println();
-	   reader.close();
-      } 
-       catch (Exception e)
-       {
-	   e.printStackTrace();
-	}
-   }
-
+       File file ;
+      Scanner fileIn;
+      int response;
+      JFileChooser chooser=new JFileChooser(".");
+      chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+      response=chooser.showOpenDialog(null);
+      if(response==JFileChooser.APPROVE_OPTION)
+      {
+    	  file=chooser.getSelectedFile();
+    	  try 
+    	  {
+		fileIn=new Scanner(file);
+		if(file.isFile())
+		{
+			while(fileIn.hasNextLine())
+			{
+			      String line=fileIn.nextLine();
+			      System.out.println(line);
+			}
+		}
+		else
+		{
+		      System.out.println("That was not a File :)");
+		}
+                fileIn.close();
+	 }
+    	  catch (FileNotFoundException e) 
+    	  {
+		
+		e.printStackTrace();
+           }
+      }
 }
