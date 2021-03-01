2c5e056d2cbb76f9667391aa68f362a10345b046
CS19B010Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2021-03-01 11:19:26 +0530

    Update simulator.java

--- simulator.java
+++ simulator.java
@@ -4,29 +4,30 @@ import java.util.*;
 public class simulator 
 {
    public static void main(String[] args) 
-	{
-	  	 Scanner sc=new Scanner(System.in);
-	   int R[]=new int[32];
+   {
+	Scanner sc=new Scanner(System.in);
+	int R[]=new int[32];
        int Mem[]=new int[1024];
        int PC=0;
        File file = new File("./cs19b010.asm");
        BufferedReader reader;
-     try {
+       try 
+       {
             reader = new BufferedReader(new FileReader(file));
-			String line = reader.readLine();
-			while (line != null) 
-			{
+	    String line = reader.readLine();
+	    while (line != null) 
+            {
 				PC=PC+4;
 				System.out.println(line);
 				line = reader.readLine();
-			}
-			System.out.println();
-			reader.close();
-     } 
-      catch (Exception e)
-      {
-			e.printStackTrace();
-		}
+	    }
+	   System.out.println();
+	   reader.close();
+      } 
+       catch (Exception e)
+       {
+	   e.printStackTrace();
 	}
+   }
 
 }
