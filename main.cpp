75a724626c3f0cbdfd587737ff7a2fd34ee8d4fa
SahithiRani <67673718+SahithiRani@users.noreply.github.com>
2021-03-01 09:46:03 +0530

    The program reads a .asm file line by line

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+int main(){
+	
+	string myText;
+
+	ifstream MyFile("que9.asm");
+
+	while (getline (MyFile, myText)) {
+	
+  			cout << myText << endl;
+	}
+
+	MyFile.close();
+
+	return 0;
+}
