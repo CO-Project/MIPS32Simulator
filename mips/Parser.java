5805246fc08dbdfdd3b039b4e010aa7b9abaf345
SahithiRani <67673718+SahithiRani@users.noreply.github.com>
2021-03-10 11:38:51 +0530

    Add files via upload

---
+++ mips/Parser.java
@@ -0,0 +1,238 @@
+package mips;
+
+import java.io.File;
+import java.util.Scanner;
+import java.util.regex.*; 
+
+public class Parser {
+
+    private File file;
+    private Scanner scanner;
+    private int lastReadLine;
+    DataSegment datasegment = new DataSegment();
+    int i=0;
+
+    public Parser(File file) {
+        try {
+            this.file = file;
+
+            lastReadLine = 0;
+            scanner = new Scanner(file);
+        } catch (Exception e) {
+            Simulator.exit("Assembler Error : Constructor : " + e.getMessage());
+        }
+    }
+   
+    public String[] nextInstruction() {
+        
+        try {
+            if (!scanner.hasNextLine())
+                return null;
+        } catch (Exception e) {
+            return null;
+        }
+
+        lastReadLine += 1;
+        String line = scanner.nextLine();
+        
+        if(line.equals(".data")){
+            lastReadLine += 1;
+            line = scanner.nextLine();
+            Pattern p = Pattern.compile("-?\\d+");
+            Matcher m = p.matcher(line);
+            while (m.find()) {
+                 int n = Integer.parseInt(m.group());
+                 datasegment.data[i] = Integer.toString(n);
+                 i++;
+            }
+            return nextInstruction();
+        }
+        if(line.equals(".text")||line.equals("main:")){
+           lastReadLine += 1;
+           line = scanner.nextLine(); 
+           return nextInstruction();
+        }
+
+        if (line.isEmpty() || line.startsWith("#"))
+            return nextInstruction();
+
+        line = line.split("#")[0];
+
+        String[] lineFormat = line.replaceAll(",", " ").split("\\s+");
+        String[] instructionFormat = findInstruction(lineFormat);
+
+        return new String[] {
+            assembleInstruction(lineFormat, instructionFormat),
+            line
+        };
+    }
+   
+    private String[] findInstruction(String[] line) {
+        try {
+           
+            Scanner stdin = new Scanner(new File("C:/Users/ASUS/eclipse-workspace/mips/src/mips/instructions.csv"));
+
+            while (stdin.hasNextLine()) {
+                String[] instruction = stdin.nextLine().split(",");
+
+                if (instruction[0].equals(line[0])) {
+                    stdin.close();
+                    return instruction;
+                }
+            }
+
+            stdin.close();
+        } catch (Exception e) {
+            Simulator.exit("Assembler Error : findInstruction : " + e.getMessage());
+        }
+
+        Simulator.exit("Assembler Error : findInstruction : Invalid Instruction Found (" + line[0] + ")");
+        return null;
+    }
+  
+    private String assembleInstruction (String[] lineFormat, String[] instructionFormat) {
+       
+        String[] instruction = new String[6];
+        int opCode = Integer.parseInt(instructionFormat[2]);
+
+        switch (instructionFormat[1]) {
+            case "R":
+               
+                instruction = new String[6];
+
+                instruction[0] = toBitString(opCode, 6);
+                instruction[1] = toBitString(Register.getRegister(lineFormat[2]), 5); 
+                instruction[2] = toBitString(Register.getRegister(lineFormat[3]), 5);
+                instruction[3] = toBitString(Register.getRegister(lineFormat[1]), 5); 
+                instruction[4] = toBitString(0, 5); 
+                instruction[5] = toBitString(Integer.parseInt(instructionFormat[3]), 6);
+
+                break;
+            case "I":
+               
+                instruction = new String[4];
+                instruction[0] = toBitString(opCode, 6);
+                instruction[1] = toBitString(Register.getRegister(lineFormat[2]), 5); 
+                instruction[2] = toBitString(Register.getRegister(lineFormat[1]), 5); 
+
+                if (opCode == 35 || opCode == 43) {
+                  
+                    if (lineFormat[2].indexOf('(') > -1) {
+                        lineFormat[2] = lineFormat[2].replace('(', ' ');
+
+                        String[] offsetFormat = lineFormat[2].split(" ");
+
+                        offsetFormat[1] = offsetFormat[1].substring(0, offsetFormat[1].length() - 1);
+
+                        instruction[1] = toBitString(Register.getRegister(offsetFormat[1]), 5); 
+                        instruction[3] = toBitString(Integer.parseInt(offsetFormat[0]), 16);
+                    } else
+                        instruction[3] = toBitString(0, 16);
+                } else
+                    instruction[3] = toBitString(Integer.parseInt(lineFormat[3]), 16);
+                break;
+            case "J":
+               
+                instruction = new String[2];
+                instruction[0] = toBitString(opCode, 6);
+                instruction[1] = toBitString(Integer.parseInt(lineFormat[1]), 26);
+
+                break;
+        }
+
+        String instructionStr = "";
+        for (String str : instruction)
+            instructionStr += str;
+
+        return instructionStr;
+    }
+    
+    public int[] decodeInstruction (String instructionRegister) {
+       
+        int opCode = Integer.parseInt(instructionRegister.substring(0, 6), 2);
+
+        if (opCode == 0) { // Check if it is R format
+            int rs = Integer.parseInt(instructionRegister.substring(6, 11), 2);
+            int rt = Integer.parseInt(instructionRegister.substring(11, 16), 2);
+            int rd = Integer.parseInt(instructionRegister.substring(16, 21), 2);
+            int func = Integer.parseInt(instructionRegister.substring(26, 32), 2);
+
+            return new int[] {
+                opCode, rs, rt, rd, 0, func
+            };
+        } else if (opCode == 2) { 
+            int address = Integer.parseInt(instructionRegister.substring(6, 32), 2);
+
+            return new int[] {
+                opCode, address
+            };
+        } else { 
+            int rs = Integer.parseInt(instructionRegister.substring(6, 11), 2);
+            int rd = Integer.parseInt(instructionRegister.substring(11, 16), 2);
+
+            int address = Integer.parseInt(instructionRegister.substring(16, 32), 2);
+
+            return new int[] {
+                opCode, rs, rd, address
+            };
+        }
+    }
+   
+    public boolean areDependent(int[] registerInstruction, int[] previousInstruction) {
+        // Check if One of the instructions is a Jump
+        if (registerInstruction[0] == 2 || previousInstruction[0] == 2)
+            return false;
+
+        for (int i = 1; i < (registerInstruction[0] == 0 ? 4 : 3); i++) {
+            if (registerInstruction[i] == 0)
+                continue;
+
+            for (int j = 1; j < (previousInstruction[0] == 0 ? 4 : 3); j++)
+                if (registerInstruction[i] == previousInstruction[j])
+                    return true;
+        }
+
+        return false;
+    }
+    
+    public void jumpTo(int programCounter) {
+        try {
+           
+            if (programCounter <= lastReadLine) {
+                scanner = new Scanner(this.file);
+                lastReadLine = 0;
+            }
+            while (scanner.hasNextLine() && lastReadLine < (programCounter - 1))
+                this.nextInstruction();
+        } catch (Exception e) {
+        	Simulator.exit("Assembler Error : jumpTo : " + e.getMessage());
+        }
+    }
+   
+    public int getProgramCounter() {
+        return lastReadLine;
+    }
+    
+    public static String toBitString(int number, int size) {
+        
+        boolean isTwosComplement = false;
+        if (number < 0)
+            isTwosComplement = true;
+        
+        String binary = Integer.toBinaryString(number);
+
+        if (binary.length() < size) {
+           
+            while (binary.length() < size)
+                binary = (isTwosComplement ? "1" : "0") + binary;
+        } else if (binary.length() > size)
+            binary = binary.substring(binary.length() - 32, binary.length());
+
+        return binary;
+    }
+    
+    public String[] getData(){
+        return datasegment.data;
+    }
+   
+}
---
+++ mips/Processor.java
@@ -0,0 +1,128 @@
+package mips;
+
+public class Processor {
+    private static Processor processor;
+
+    private Memory memory;
+    private Register register;
+    private DataSegment datasegment = new DataSegment();
+
+   
+    public Processor () {
+        memory   = Memory.getInstance();
+        register = Register.getInstance();
+    }
+   
+    public static synchronized Processor getInstance() {
+        if (processor == null)
+            processor = new Processor();
+        return processor;
+    }
+    
+    public int executeInstruction(int[] instruction, DataSegment data) {
+        
+        datasegment = data;
+
+        int opCode = instruction[0];
+        if (opCode == 0) {
+            int rs = instruction[1];
+            int rt = instruction[2];
+            int rd = instruction[3];
+
+            int func = instruction[5];
+
+            switch (func) {
+                case 32:
+                    processor.add(rd, rs, rt);
+                    break;
+                case 34:
+                    processor.sub(rd, rs, rt);
+                    break;
+                case 36:
+                    processor.and(rd, rs, rt);
+                    break;
+                case 37:
+                    processor.or(rd, rs, rt);
+                    break;
+                case 42:
+                    processor.slt(rd, rs, rt);
+                    break;
+            }
+
+            return -1;
+        } else if (opCode == 2) {
+            return instruction[1];
+        } else {
+            int rs = instruction[1];
+            int rd = instruction[2];
+
+            int address = instruction[3];
+
+            switch (opCode) {
+                case 4:
+                    return processor.beq(rd, rs, address);
+                case 5:
+                    return processor.bne(rd, rs, address);
+                case 8:
+                	processor.addi(rd, rs, address);
+                    break;
+                case 35:
+                	processor.lw(rd, rs, address);
+                    break;
+                case 43:
+                	processor.sw(rd, rs, address);
+                    break;
+            }
+
+            return -1;
+        }
+    }
+   
+    private void add(int rd, int rs, int rt) {
+        register.write(rd, register.read(rs) + register.read(rt));
+    }
+
+    private void sub(int rd, int rs, int rt) {
+        register.write(rd, register.read(rs) - register.read(rt));
+    }
+
+    private void and(int rd, int rs, int rt) {
+        register.write(rd, register.read(rs) & register.read(rt));
+    }
+
+    private void or(int rd, int rs, int rt) {
+        register.write(rd, register.read(rs) | register.read(rt));
+    }
+
+    private void slt(int rd, int rs, int rt) {
+        boolean isSlt = register.read(rs) < register.read(rt);
+
+        register.write(rd, (isSlt) ? 1 : 0);
+    }
+  
+    private int beq(int rs, int rt, int address) {
+        if (register.read(rs) == register.read(rt))
+            return address;
+        else return -1;
+    }
+
+    private int bne(int rs, int rt, int address) {
+        if (register.read(rs) != register.read(rt))
+            return address;
+        else return -1;
+    }
+
+    private void addi(int rd, int rs, int constant) {
+        register.write(rd, register.read(rs) + constant);
+    }
+
+    private void lw(int rd, int rs, int address) {
+        register.write(rd, datasegment.read((register.read(rs)) + address));
+    }
+
+    private void sw(int rd, int rs, int address) {
+       
+        datasegment.write(register.read(rs) + address, register.read(rd));
+    }
+   
+}
